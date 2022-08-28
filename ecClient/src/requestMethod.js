0de6e02406a433c9f5508c2a98acaa173164d0f5
Heliumcxxi <Heliumcxxi@users.noreply.github.com>
2022-08-28 12:21:32 -0400

    fix base url

--- ecClient/src/requestMethod.js
+++ ecClient/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://jquest001.herokuapp.com/";
+const BASE_URL = "https://ecbackend1.herokuapp.com";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYWY3MzcxNjg3NTVhOGFhNTVhZDdmZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NjA4MzM3OX0.JHFwFsk4etoZrs96IC18YsrTiAV5HyspCEuN18vtrEs";
 
